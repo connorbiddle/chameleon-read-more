90adcf21810d751bfc87f2c2fbd60637c4ccbf00
Connor <connor.biddle@hotmail.com>
2025-01-02 15:45:15 +0000

    Initial commit. Added plugin header.

---
+++ chameleon-read-more.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Plugin Name:       Chameleon Read More
+ * Description:       Add a "read more" link to specified Text Editor modules.
+ * Version:           0.0.1
+ * Author:            Chameleon
+ * Author URI:        https://chameleon.co.uk/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
